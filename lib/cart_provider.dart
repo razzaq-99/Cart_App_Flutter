09d30d873564159744f12ff2918320cf7ba58c6e
abdul razzaq <ar8937562@gmail.com>
2023-08-02 09:01:27 -0700

    updated files to manage product price and item and changes in the cart

---
+++ lib/cart_provider.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartProvider with ChangeNotifier {
+  int _counter = 0;
+  int get counter => _counter;
+
+  double _totalPrice = 0.0;
+  double get totalPrice => _totalPrice;
+
+  void _setPrefItems() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setInt('cart_item', _counter);
+    preferences.setDouble('total_price', _totalPrice);
+    notifyListeners();
+  }
+
+  void _getPrefItems() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    _counter = preferences.getInt('cart_item') ?? 0;
+    _totalPrice = preferences.getDouble('total_price') ?? 0.0;
+    notifyListeners();
+  }
+
+  void addTotalPrice(double productPrice) {
+    _totalPrice = _totalPrice + productPrice;
+    _setPrefItems();
+    notifyListeners();
+  }
+
+  void removeTotalPrice(double productPrice) {
+    _totalPrice = _totalPrice - productPrice;
+    _setPrefItems();
+    notifyListeners();
+  }
+
+  double getTotalPrice() {
+    _getPrefItems();
+    return _totalPrice;
+  }
+
+  void addCounter() {
+    _counter++;
+    _setPrefItems();
+    notifyListeners();
+  }
+
+  void removeCounter() {
+    _counter--;
+    _setPrefItems();
+    notifyListeners();
+  }
+
+  int getCounter() {
+    _getPrefItems();
+    return _counter;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cart_app/cart_provider.dart';
 import 'package:cart_app/product_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,22 +13,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const ProductList(),
+    return ChangeNotifierProvider(
+      create: (_) => CartProvider(),
+      child: Builder(builder: (BuildContext context) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const ProductList(),
+        );
+      }),
     );
   }
 }
